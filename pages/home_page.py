a25d5f4addb94b670b60229de722d61e3aff20b8
SEtester <460060639@qq.com>
2020-01-19 17:26:21 +0800

    Update code style

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,11 +1,12 @@
 from base_page import BasePage, By
 
-class HomePage(BasePage):
 
-    user_title_locator =(By.CSS_SELECTOR,'.avatar-text-default.avatar-Q')
+class HomePage(BasePage):
+    user_title_locator = (By.CSS_SELECTOR, '.avatar-text-default.avatar-Q')
 
     def user_title(self):
         return self.find_element(self.user_title_locator)
+
 
 class HomePageAction(HomePage):
 
