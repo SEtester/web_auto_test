4f8238dbc7c5d376e003f58df68e3e261a5e3701
SEtester <460060639@qq.com>
2020-01-19 18:38:19 +0800

    Add run_login_case.py soucre file

--- run_case/run_login_case.py
+++ run_case/run_login_case.py
@@ -1,8 +1,7 @@
-
-
 import unittest
 from test_case.test_login import TestLoginCase
 from utils.se_utils import Driver
+
 if __name__ == '__main__':
     cases = unittest.TestLoader().loadTestsFromTestCase(TestLoginCase)
     runner = unittest.TextTestRunner(verbosity=2)
