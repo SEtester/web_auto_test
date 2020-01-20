aa868fd8637af4f064c100ddc98f159d49bea75c
SEtester <460060639@qq.com>
2020-01-20 15:24:37 +0800

    Add module_path.py source file -- python sys.path

---
+++ run_case/module_path.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+web_auto_test_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+pages_dir_path = web_auto_test_path + '/pages'
+config_dir_path = web_auto_test_path + '/config'
+run_case_dir_path = web_auto_test_path + '/run_case'
+test_case_dir_path = web_auto_test_path + '/test_case'
+utils_dir_path = web_auto_test_path + '/utils'
+
+sys.path.extend([
+    web_auto_test_path,
+    pages_dir_path,
+    config_dir_path,
+    run_case_dir_path,
+    test_case_dir_path,
+    utils_dir_path,
+])
+
+from pprint import pprint
+pprint('python引包路径:',sys.path)
